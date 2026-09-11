bcd5d79d5e60bc35e5e2c8ef72a1702683db972d
Vinay Sharma <vinay74sharmakush@gmail.com>
2026-09-11 21:50:50 +0530

    Complete CRUD operations for Quora posts

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -12,17 +15,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
  {
-    id:"1a",
+    id: uuidv4(),
     username : "apnacollege",
     content : "I love coding!"
 },
  {
-    id:"2b",
+    id:uuidv4(),
     username : "VinaySharma",
     content : "HardWork is Important to achieve success!"
 },
  {
-    id:"3c",
+    id:uuidv4(),
     username : "Taniya",
     content : "I got selected for my first internship!"
 },
@@ -41,7 +44,8 @@ app.get("/posts/new" , (req, res) => {
 
 app.post("/posts", (req, res) => {
     let {username, content } = req.body;
-    posts.push({ username , content })
+    let id = uuidv4();
+    posts.push({ id, username , content })
     res.redirect("/posts");
 });
 
@@ -49,6 +53,28 @@ app.get("/posts/:id", (req,res) => {
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs", { post });
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.send("patch request working");
+    res.redirect("/posts");
+});
+
+app.get ("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
 })
 
 app.listen(port, () => {
